059976b5b4b02a29de637751ab20212bfb81ab73
gywlsp <gywls517@gmail.com>
2020-09-06 23:49:38 +0900

    feat: :lipstick: change Typography

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,17 +1,24 @@
 import Typography from "typography"
-import Wordpress2016 from "typography-theme-wordpress-2016"
 
-Wordpress2016.overrideThemeStyles = () => {
-  return {
-    "a.gatsby-resp-image-link": {
-      boxShadow: `none`,
+const typography = new Typography({
+  baseFontSize: "18px",
+  baseLineHeight: 1.618,
+  googleFonts: [
+    {
+      name: "Noto Sans KR",
+      styles: ["300", "400", "500", "700"],
     },
-  }
-}
-
-delete Wordpress2016.googleFonts
-
-const typography = new Typography(Wordpress2016)
+    {
+      name: "Nanum Gothic KR",
+      styles: ["400", "700"],
+    },
+  ],
+  headerFontFamily: ["Noto Sans KR", "sans-serif"],
+  bodyFontFamily: ["Nanum Gothic", "sans-serif"],
+  headerWeight: 700,
+  bodyWeight: 400,
+  boldWeight: 700,
+})
 
 // Hot reload typography in development.
 if (process.env.NODE_ENV !== `production`) {
