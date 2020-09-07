d57cc16f4f74df35097873e48f49fdb307ad1aa6
gywlsp <gywls517@gmail.com>
2020-09-07 22:54:29 +0900

    feat: :sparkles: add page layout

---
+++ src/components/templates/layout.tsx
@@ -0,0 +1,32 @@
+import React, { ReactChild } from 'react';
+import styled from 'styled-components';
+
+import GNB from '../organisms/GNB';
+
+export type LayoutProps = {
+  children: ReactChild;
+  selectedTag?: string;
+};
+
+function Layout({ children, selectedTag }) {
+  return (
+    <Wrapper>
+      <GNB selectedTag={selectedTag} />
+      <Main>{children}</Main>
+    </Wrapper>
+  );
+}
+
+export default React.memo(Layout);
+
+const Wrapper = styled.div`
+  display: flex;
+  width: 100%;
+  min-height: 100vh;
+`;
+
+const Main = styled.main`
+  flex: 1;
+  padding: 3rem;
+  min-height: 100vh;
+`;
