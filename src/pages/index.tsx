5bda7acdd41a8e8612e12da660838b698a1bde8e
gywlsp <gywls517@gmail.com>
2020-09-07 22:56:44 +0900

    feat: :sparkles: make page filtered by selected tag

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,50 +1,48 @@
-import React from "react"
-import { Link, graphql } from "gatsby"
+import React from 'react';
+import { Link, graphql } from 'gatsby';
 
-import Bio from "../components/bio"
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-import { rhythm } from "../utils/typography"
+import Layout from '../components/templates/layout';
+import SEO from '../components/seo';
+import withLocation from '../withLocation';
 
-const BlogIndex = ({ data, location }) => {
-  const siteTitle = data.site.siteMetadata.title
-  const posts = data.allMarkdownRemark.edges
+function HomePage({ data, location, search }) {
+  const posts = data.allMarkdownRemark.edges;
+  const { tag } = search;
 
   return (
-    <Layout location={location} title={siteTitle}>
-      <SEO title="All posts" />
-      <Bio />
-      {posts.map(({ node }) => {
-        const title = node.frontmatter.title || node.fields.slug
-        return (
-          <article key={node.fields.slug}>
-            <header>
-              <h3
-                style={{
-                  marginBottom: rhythm(1 / 4),
-                }}
-              >
-                <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
-                  {title}
-                </Link>
-              </h3>
-              <small>{node.frontmatter.date}</small>
-            </header>
-            <section>
-              <p
-                dangerouslySetInnerHTML={{
-                  __html: node.frontmatter.description || node.excerpt,
-                }}
-              />
-            </section>
-          </article>
+    <Layout selectedTag={tag || (location.pathname = '/' ? 'ALL' : undefined)}>
+      <SEO title="Home" />
+      {posts
+        .filter(({ node }) =>
+          tag ? node.frontmatter.tags?.includes(tag) : true
         )
-      })}
+        .map(({ node }) => {
+          const title = node.frontmatter.title || node.fields.slug;
+          return (
+            <article key={node.fields.slug}>
+              <header>
+                <h3>
+                  <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
+                    {title}
+                  </Link>
+                </h3>
+                <small>{node.frontmatter.date}</small>
+              </header>
+              <section>
+                <p
+                  dangerouslySetInnerHTML={{
+                    __html: node.frontmatter.description || node.excerpt
+                  }}
+                />
+              </section>
+            </article>
+          );
+        })}
     </Layout>
-  )
+  );
 }
 
-export default BlogIndex
+export default withLocation(HomePage);
 
 export const pageQuery = graphql`
   query {
@@ -64,9 +62,10 @@ export const pageQuery = graphql`
             date(formatString: "MMMM DD, YYYY")
             title
             description
+            tags
           }
         }
       }
     }
   }
-`
+`;
