03abd3780b8faab556e211e28ebb62b888c86c3a
gywlsp <gywls517@gmail.com>
2020-09-07 20:41:18 +0900

    chore: :wrench: set eslint explicit-module-boundary-types off

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
   },
   rules: {
     '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-unused-vars': 'off',
     'prettier/prettier': 0,
     '@typescript-eslint/no-var-requires': 'off',
