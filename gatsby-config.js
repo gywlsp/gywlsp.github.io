d8570f29af158eae452fa6e703fe2b6610ecec78
gywlsp <gywls517@gmail.com>
2020-09-08 00:44:05 +0900

    chore: :wrench: set auto import

--- gatsby-config.js
+++ gatsby-config.js
@@ -70,7 +70,8 @@ module.exports = {
       }
     },
     `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-typescript`
+    `gatsby-plugin-typescript`,
+    'gatsby-plugin-root-import'
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
