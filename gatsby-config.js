7d62d947b466d3b7098413dc885f74bb010a45fa
gywlsp <gywls517@gmail.com>
2020-09-09 22:35:50 +0900

    build: :package: add gatsby-plugin-styled-components

--- gatsby-config.js
+++ gatsby-config.js
@@ -68,7 +68,10 @@ module.exports = {
     },
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-typescript`,
-    'gatsby-plugin-root-import'
+    'gatsby-plugin-root-import',
+    {
+      resolve: `gatsby-plugin-styled-components`
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
