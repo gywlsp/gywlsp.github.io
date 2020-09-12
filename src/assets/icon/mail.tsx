fe1878b6cdf9f00e95fb4355bfe03644e33b8130
gywlsp <gywls517@gmail.com>
2020-09-12 23:58:12 +0900

    chore: :bento: add icons
    logo, mail

--- src/assets/icon/mail.tsx
+++ src/assets/icon/mail.tsx
@@ -13,10 +13,6 @@ export default function MailIcon({ style = {}, fill = BLACK }: IconProps) {
             <path
                 fill={fill}
                 d="M 0 7 L 0 8 L 0 43 L 50 43 L 50 7 L 0 7 z M 2 9 L 48 9 L 48 41 L 2 41 L 2 9 z M 4 13.916016 L 4 16.275391 L 25 28.179688 L 46 16.275391 L 46 13.916016 L 25 25.820312 L 4 13.916016 z"
-                font-weight="400"
-                font-family="sans-serif"
-                white-space="normal"
-                overflow="visible"
             />
         </svg>
     );
