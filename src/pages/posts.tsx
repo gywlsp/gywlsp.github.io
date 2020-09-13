34a49f823cfaa5f28e0df8a1f17699f75f811373
gywlsp <gywls517@gmail.com>
2020-09-13 16:51:49 +0900

    feat: :sparkles: add PageTitle to postst page

--- src/pages/posts.tsx
+++ src/pages/posts.tsx
@@ -1,29 +1,42 @@
 import React from 'react';
 import { graphql } from 'gatsby';
-
-import Layout from 'src/components/templates/layout';
-import SEO from 'src/components/seo';
-import PostPreviewCard from 'src/components/organisms/post/preview';
+import styled from 'styled-components';
 
 import withLocation from 'src/withLocation';
+import Layout from 'src/components/templates/layout';
+import SEO from 'src/components/templates/seo';
+import PostPreviewCard from 'src/components/molecules/post/preview';
+import { BLUE } from 'src/constants/colors';
 
 function PostsPage({ data, location, search }) {
-  const posts = data.allMarkdownRemark.edges;
+  const posts = data?.allMarkdownRemark.edges;
+
   const { tag } = search;
 
   return (
     <Layout selectedTag={tag || 'ALL'} pathname={location.pathname}>
       <SEO title="posts" />
-      {posts
-        .filter(({ node }) =>
-          tag ? node.frontmatter.tags?.includes(tag) : true
-        )
-        .map(({ node }) => <PostPreviewCard key={JSON.stringify(node)} {...node} />)}
+      <PageTitle>
+        POSTS<strong style={{ color: BLUE }}>{tag ? ` - ${tag}` : ''}</strong>
+      </PageTitle>
+      <PostsWrapper>
+        {posts
+          .filter(({ node }) =>
+            tag ? node.frontmatter.tags?.includes(tag) : true
+          )
+          .map(({ node }) => (
+            <PostPreviewCard
+              key={JSON.stringify(node)}
+              {...node}
+              thumbnail={node.frontmatter.thumbnail?.childImageSharp.fluid}
+            />
+          ))}
+      </PostsWrapper>
     </Layout>
   );
 }
 
-export default withLocation(PostsPage);
+export default withLocation(React.memo(PostsPage));
 
 export const pageQuery = graphql`
   query {
@@ -44,9 +57,30 @@ export const pageQuery = graphql`
             title
             description
             tags
+            thumbnail {
+              childImageSharp {
+                fluid(maxWidth: 800) {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
           }
         }
       }
     }
   }
+`;
+
+const PageTitle = styled.p`
+  margin: 0;
+  margin-bottom: 0.8rem;
+  font-size: 2.8rem;
+  font-weight: 700;
+`;
+
+const PostsWrapper = styled.div`
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: space-between;
+  width: 100%;
 `;
