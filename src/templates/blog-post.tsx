60f1a967ead7ab311138005356165a3711cc884c
gywlsp <gywls517@gmail.com>
2020-09-13 17:02:12 +0900

    feat: :mag: add metadata to blog-post template

--- src/templates/blog-post.tsx
+++ src/templates/blog-post.tsx
@@ -1,36 +1,61 @@
 import React from 'react';
 import { Link, graphql } from 'gatsby';
-import moment from 'moment';
+import Image from 'gatsby-image';
 import styled from 'styled-components';
+import moment from 'moment';
 
 import Layout from 'src/components/templates/layout';
-import Bio from 'src/components/bio';
-import SEO from 'src/components/seo';
+import SEO, { getSchemaOrgJSONLD } from 'src/components/templates/seo';
+import { Tag } from 'src/components/atoms';
 import {
   MIDDLE_GREY,
   BLACK,
-  BLUE,
-  WHITE,
+  LIGHT_GREY,
 } from 'src/constants/colors';
 
 function BlogPostTemplate({ data, pageContext, location }) {
   const post = data.markdownRemark;
+  const siteTitle = data.site.siteMetadata.title;
   const { previous, next } = pageContext;
+
+  const { excerpt } = post;
+  const { slug } = post.fields;
+  const { title, date, thumbnail } = post.frontmatter;
+  const url = `${data.site.siteMetadata.siteUrl}${slug}`;
+  const image = thumbnail.childImageSharp.fluid.src;
+
+  const schemaOrgJSONLD = getSchemaOrgJSONLD({
+    url,
+    title,
+    image,
+    description: excerpt,
+    datePublished: date,
+    siteTitle,
+    siteUrl: data.site.siteMetadata.siteUrl,
+  });
 
   return (
     <Layout selectedTag={undefined} pathname={location.pathname}>
       <SEO
         title={post.frontmatter.title}
         description={post.frontmatter.description || post.excerpt}
-      />
+        meta={[
+          { name: 'image', content: image },
+          { name: 'og:image', content: image },
+          { name: 'og:type', content: 'article' },
+          { name: 'og:url', content: url },
+        ]}
+      >
+        <script type="application/ld+json">
+          {JSON.stringify(schemaOrgJSONLD)}
+        </script>
+      </SEO>
       <Article>
         <header>
           <H1>{post.frontmatter.title}</H1>
           <Row>
             {post.frontmatter.tags.map((tag) => (
-              <TagWrapper>
-                <Tag>{tag}</Tag>
-              </TagWrapper>
+              <Tag key={tag} title={tag} />
             ))}
           </Row>
           <Row>
@@ -48,10 +73,9 @@ function BlogPostTemplate({ data, pageContext, location }) {
         />
         <hr />
         <footer>
-          <Bio />
+          {/* <Bio /> */}
         </footer>
       </Article>
-
       <nav>
         <ul
           style={{
@@ -89,15 +113,19 @@ export const pageQuery = graphql`
     site {
       siteMetadata {
         title
+        siteUrl
       }
     }
     markdownRemark(fields: { slug: { eq: $slug } }) {
       id
       excerpt(pruneLength: 160)
       html
+      fields {
+        slug
+      }
       frontmatter {
         title
-        date(formatString: "MMMM DD, YYYY")
+        date
         description
         tags
       }
@@ -111,6 +139,7 @@ const Article = styled.article`
 
 const H1 = styled.h1`
   && {
+    margin: 0;
     margin-bottom: 0.8rem;
     font-size: 2.8rem;
   }
@@ -137,15 +166,4 @@ const StyledLink = styled(Link)`
   box-shadow: none;
   text-decoration: none;
   color: ${BLACK};
-`;
-
-const TagWrapper = styled.div`
-  padding: 0.4rem 0.8rem;
-  background-color: ${BLUE};
-`;
-
-const Tag = styled.p`
-  font-size: 1.4rem;
-  color: ${WHITE};
-  margin: 0;
 `;
