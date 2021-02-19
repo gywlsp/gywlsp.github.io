6290be7b2c58c640c9c275f084d2f4756f37659b
gywlsp <gywls517@gmail.com>
2021-02-19 22:52:32 +0900

    feat: :sparkles: add Recent Posts section to home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,36 @@
 import React from 'react';
+import styled from 'styled-components';
+import { Link } from "gatsby"
 import { graphql } from 'gatsby';
 
 import Layout from 'src/components/templates/layout';
 import SEO from 'src/components/templates/seo';
+import PostPreviewCard from 'src/components/molecules/post-card/preview';
+import ChevronRightIcon from 'src/assets/icon/chevron/right';
 
 import withLocation from 'src/withLocation';
 
-function MainPage({ data, location, search }) {
-  const posts = data.allMarkdownRemark.edges;
-  const { tag } = search;
+function MainPage({ data, location }) {
+  const posts = data.allMarkdownRemark.edges.slice(0, 8);
 
   return (
-    <Layout selectedTag={tag || 'ALL'} pathname={location.pathname}>
+    <Layout selectedTag="ALL" pathname={location.pathname}>
       <SEO title="main" />
+      <Row>
+        <PageTitle>RECENT POSTS</PageTitle>
+        <Link to="/posts">
+          <ChevronRightIcon />
+        </Link>
+      </Row>
+      <PostsWrapper>
+        {posts.map(({ node }) => (
+          <PostPreviewCard
+            key={JSON.stringify(node)}
+            {...node}
+            thumbnail={node.frontmatter.thumbnail?.childImageSharp.fluid}
+          />
+        ))}
+      </PostsWrapper>
     </Layout>
   );
 }
@@ -38,9 +56,42 @@ export const pageQuery = graphql`
             title
             description
             tags
+            thumbnail {
+              childImageSharp {
+                fluid(maxWidth: 800) {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
           }
         }
       }
     }
   }
+`;
+
+const Row = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  margin-bottom: 0.8rem;
+`;
+
+const PageTitle = styled.p`
+  margin: 0;
+  font-size: 2.8rem;
+  font-weight: 700;
+`;
+
+const A = styled.a`
+  margin: 0;
+  text-decoration: none;
+`;
+
+const PostsWrapper = styled.div`
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: space-between;
+  width: 100%;
 `;
