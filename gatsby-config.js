6ff3fad65fc9dad8fa5c3e5ce9f95bb15ddaae97
gywlsp <gywls517@gmail.com>
2021-02-20 02:25:18 +0900

    build: :wrench: make gatsby-plugin-mdx not applied to md files

--- gatsby-config.js
+++ gatsby-config.js
@@ -48,7 +48,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-mdx`,
       options: {
-        extensions: [`.mdx`, `.md`]
+        extensions: [`.mdx`]
       },
     },
     `gatsby-transformer-sharp`,
