20092c9561f2c65d30808c0be67716493229a48c
gywlsp <gywls517@gmail.com>
2021-02-25 12:41:53 +0900

    feat: :sparkles: update about-me component
    add blog description

--- src/components/organisms/about-me.tsx
+++ src/components/organisms/about-me.tsx
@@ -2,32 +2,71 @@ import React from 'react';
 import styled from 'styled-components';
 import { MDXProvider } from '@mdx-js/react';
 
+import { Img as AtomImage } from 'src/components/atoms';
 import AboutMeMd from 'src/contents/about-me.mdx';
+import { POINT_BLUE, SKY_BLUE } from 'src/constants/colors';
 
 export default function AboutMe() {
   return (
     <MDXProvider
       components={{
-        h1: H1,
         h2: H2,
+        h3: H3,
         img: Img,
         ul: Ul,
         li: Li,
       }}
     >
+      <Wrapper>
+        <AtomImage src="/images/logo.svg" width="4rem" alt="logo" />
+        <TitleWrapper>
+          <Title>사이다 데브-로그</Title>
+          <Description>
+            사이다를 마신 것 같이 시원하게 이해가 잘 되는 글들로 이루어진{' '}
+            <Strong>기술 블로그</Strong>입니다.
+          </Description>
+        </TitleWrapper>
+      </Wrapper>
       <AboutMeMd />
     </MDXProvider>
   );
 }
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 2rem;
+`;
 
-export const H1 = styled.h1`
-  font-size: 2.4rem;
+const TitleWrapper = styled.div`
+  margin-left: 0.8rem;
+`;
+
+const Title = styled.h1`
   margin: 0;
-  margin-bottom: 1.6rem;
+  font-family: 'paybooc-Bold';
+  font-size: 1.8rem;
+`;
+
+const Description = styled.p`
+  margin: 0;
+  font-size: 1.2rem;
+  color: ${SKY_BLUE};
+`;
+
+const Strong = styled.strong`
+  color: ${POINT_BLUE};
+  font-weight: 400;
 `;
 
 export const H2 = styled.h2`
   font-size: 2rem;
+  font-weight: 700;
+  margin: 0;
+  margin-bottom: 1.6rem;
+`;
+
+export const H3 = styled.h3`
+  font-size: 1.8rem;
   font-weight: 700;
   margin: 0;
   margin-bottom: 0.8rem;
