caff3704555e7b70508c981d79826e8b5da32abd
gywlsp <gywls517@gmail.com>
2021-03-18 17:43:17 +0900

    feat: :sparkles: add useScrollDirection hook

---
+++ src/hooks/utils.ts
@@ -0,0 +1,20 @@
+import { useState, useEffect } from 'react';
+
+export const useScrollDirection = (initialDirection: string) => {
+  const [direction, setDirection] = useState(initialDirection);
+  const [position, setPosition] = useState(0);
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY < 100) return;
+      if (Math.abs(position - window.scrollY) > 15) {
+        setDirection(position > window.scrollY ? 'up' : 'down');
+        setPosition(window.scrollY);
+      }
+    };
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  });
+  return direction;
+};
