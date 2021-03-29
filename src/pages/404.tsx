9341e9ad2b7409415f6f1b52619a9dcf82a9a979
gywlsp <gywls517@gmail.com>
2021-03-29 22:30:28 +0900

    refactor: :recycle: update NotFoundPage

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,29 +1,16 @@
 import React from 'react';
-import { graphql } from 'gatsby';
 
-import Layout from 'src/components/layout';
+import Layout from 'src/components/templates/layout';
 import SEO from 'src/components/templates/seo';
 
-const NotFoundPage = ({ data, location }) => {
-  const siteTitle = data.site.siteMetadata.title;
-
+function NotFoundPage({ location }) {
   return (
-    <Layout location={location} title={siteTitle}>
+    <Layout pathname={location.pathname}>
       <SEO title="404: Not Found" />
       <h1>Not Found</h1>
       <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
     </Layout>
   );
-};
+}
 
 export default NotFoundPage;
-
-export const pageQuery = graphql`
-  query {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-  }
-`;
