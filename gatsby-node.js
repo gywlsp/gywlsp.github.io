dc77ef1939d815ea975be02b917df2378576e506
gywlsp <gywls517@gmail.com>
2021-04-08 21:49:36 +0900

    refactor: :recycle: refactor post page creating logic

--- gatsby-node.js
+++ gatsby-node.js
@@ -35,14 +35,15 @@ exports.createPages = async ({ graphql, actions }) => {
   const posts = result.data.allMarkdownRemark.edges;
 
   posts.forEach((post, index) => {
-    const previous = index === posts.length - 1 ? null : posts[index + 1].node;
-    const next = index === 0 ? null : posts[index - 1].node;
+    const { slug } = post.node.fields;
+    const previous = posts[index + 1]?.node;
+    const next = posts[index - 1]?.node;
 
     createPage({
-      path: post.node.fields.slug,
+      path: slug,
       component: blogPost,
       context: {
-        slug: post.node.fields.slug,
+        slug,
         previous,
         next,
       },
